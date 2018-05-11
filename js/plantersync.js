c6885c06f93a3a1eda7eec0a33fc1ceffe443877
John Andersen <johnandersenpdx@gmail.com>
2018-05-10 20:24:55 -0700

    plantersync: Fix for phones not re-requesting
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- js/plantersync.js
+++ js/plantersync.js
@@ -10,7 +10,17 @@ class PlanterSync extends Sync {
     // "passive resource", an Image load, which the server running on the
     // planter will accept and use the parameters to connect to WiFi.
     // https://developer.mozilla.org/en-US/docs/Web/Security/Mixed_content
+    var resolved = false;
+    const found = function() {
+      if (resolved === false) {
+        resolved = true;
+        resolve(resource);
+      }
+    }.bind(this);
     const bound = function() {
+      setTimeout(function() {
+        bound(resource, resolve, reject);
+      }.bind(this), 5000);
       var image = new Image();
       image.onerror = function() {
         // TODO Figure out when we need to set timeout and when the browser will
@@ -18,7 +28,7 @@ class PlanterSync extends Sync {
         setTimeout(bound(resource, resolve, reject), 5000);
       }.bind(this);
       image.onload = function() {
-        resolve(resource);
+        found();
       }.bind(this);
       image.src = this.value + '/apiwifisave?s={0}&p={1}&t={2}'.format(
           resource.value.SSID, resource.value.password, resource.value.token);
