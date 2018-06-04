8ab5a619f55df2fe12ffc0a134937838eae364b5
John Andersen <johnandersenpdx@gmail.com>
2018-06-04 09:01:22 -0700

    scripts: Fixed setup.sh symbolic link
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -18,4 +18,4 @@ find . -type f -not -path './api/vendor/*' -name \*.php -exec chmod -v 755 {} \;
 find . -type d -not -path './api/vendor/*' -exec chmod -v 755 {} \;
 cd ..
 npm run webpack
-ln -s $PWD/dist/ ~/public_html/smartplanter/
+ln -s $PWD/dist ../smartplanter
